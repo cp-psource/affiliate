b02a94e62ccad8c3ea21b6dbb6225452a056a55b
Dernerd <dern3rd@outlook.at>
2021-09-18 22:51:42 +0200

    Anpassungen

--- affiliate.php
+++ affiliate.php
@@ -8,7 +8,7 @@ Version: 3.2.5
 Author URI: https://n3rds.work
 Domain Path: /affiliateincludes/languages
 */
-require 'affiliateincludes/psource-plugin-update/plugin-update-checker.php';
+require 'psource/psource-plugin-update/plugin-update-checker.php';
 $MyUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	'https://n3rds.work//wp-update-server/?action=get_metadata&slug=affiliate', 
 	__FILE__, 
